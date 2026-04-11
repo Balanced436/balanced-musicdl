b2f20e165c26dd38b09ef39906f987053e38a780
SAID <ismael.saidpro@protonmail.com>
2026-04-11 19:02:27 +0400

    [feat] add accoustid as a secret

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -4,9 +4,11 @@
 echo "" > .env
 echo "" > .env.test
 
-POSTGRES_USER=$(cat /run/secrets/postgres_user)
-POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
-POSTGRES_DB=$(cat /run/secrets/postgres_db)
+POSTGRES_USER=$(cat "${POSTGRES_USER_FILE}")
+POSTGRES_PASSWORD=$(cat "${POSTGRES_PASSWORD_FILE}")
+POSTGRES_DB=$(cat "${POSTGRES_DB_FILE}")
+ACCOUSTID_CLIENT=$(cat "${ACCOUSTID_CLIENT_FILE}")
+
 DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}?schema=public"
 echo "DATABASE_URL=$DATABASE_URL" >> .env
 
@@ -16,8 +18,7 @@ POSTGRES_DB_TEST="${POSTGRES_DB}_test"
 DATABASE_URL_TEST="postgresql://${POSTGRES_USER_TEST}:${POSTGRES_PASSWORD_TEST}@${POSTGRES_HOST}:5432/${POSTGRES_DB_TEST}?schema=public"
 echo "DATABASE_URL=$DATABASE_URL_TEST" >> .env.test
 
-echo "SONGS_DIR=$SONGS_DIR" >> .env
-echo "SONGS_DIR=$SONGS_DIR" >> .env.test
+echo "ACCOUSTID_CLIENT=$ACCOUSTID_CLIENT" >> .env
 mkdir -p "$SONGS_DIR"
 
 npm install
