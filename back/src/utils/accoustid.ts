9e19d2abc93b2f6bb65b1d695a9fb6f2bd3ded93
SAID <ismael.saidpro@protonmail.com>
2026-04-14 20:27:15 +0400

    [reactor] add musicBrainzAlbumCover, remove musicBrainz utils from accoustid.ts, fix meta params

--- back/src/utils/accoustid.ts
+++ back/src/utils/accoustid.ts
@@ -53,18 +53,11 @@ interface AcoustIDResponse {
   }>;
 }
 
-export const getMBIDRecords = async (
+export const accoustidLookup = async (
   accoustidClient: string,
   fingerPrint: fingerPrint,
 ): Promise<AcoustIDResponse> => {
   const { duration, fingerprint } = fingerPrint;
-  const client = process.env.client;
-
-  if (!client) {
-    throw new Error(
-      "AcoustID client key is missing from environment variables",
-    );
-  }
 
   const url = new URL("https://api.acoustid.org/v2/lookup");
   url.searchParams.set("client", accoustidClient);
@@ -88,57 +81,3 @@ export const getMBIDRecords = async (
 
   return response;
 };
-
-export interface MusicBrainzArtist {
-  id: string;
-  name: string;
-  "sort-name": string;
-  disambiguation?: string;
-}
-
-export interface MusicBrainzRelease {
-  id: string;
-  title: string;
-  "status-id"?: string;
-  status?: string;
-  date?: string;
-  "country"?: string;
-  "quality"?: string;
-}
-
-export interface MusicBrainzRecording {
-  id: string;
-  title: string;
-  length: number | null;
-  video: boolean;
-  disambiguation?: string;
-  "artist-credit"?: Array<{
-    artist: MusicBrainzArtist;
-    name: string;
-    joinphrase?: string;
-  }>;
-  releases?: MusicBrainzRelease[];
-}
-
-export const getMusicBrainzRecords = async (mbid: string): Promise<MusicBrainzRecording> => {
-  const url = new URL(`https://musicbrainz.org/ws/2/recording/${mbid}`);
-  url.searchParams.set("fmt", "json");
-
-  const requestOptions = {
-    headers: {
-      "User-Agent": "Balancedmusicdl/1.0.0 ( dev@isaid.dev )",
-      "Accept": "application/json"
-    }
-  };
-
-  const request = await fetch(url.toString(), requestOptions);
-
-  if (!request.ok) {
-    throw new Error(
-        `MusicBrainz API error: ${request.status} ${request.statusText}`,
-    );
-  }
-
-  const response: MusicBrainzRecording = await request.json();
-  return response;
-}
---
+++ back/src/utils/musicBrainz.ts
@@ -0,0 +1,60 @@
+export interface MusicBrainzArtist {
+  id: string;
+  name: string;
+  "sort-name": string;
+  disambiguation?: string;
+}
+
+export interface MusicBrainzRelease {
+  id: string;
+  title: string;
+  "status-id"?: string;
+  status?: string;
+  date?: string;
+  country?: string;
+  quality?: string;
+}
+
+export interface MusicBrainzRecording {
+  id: string;
+  title: string;
+  length: number | null;
+  video: boolean;
+  disambiguation?: string;
+  "artist-credit"?: Array<{
+    artist: MusicBrainzArtist;
+    name: string;
+    joinphrase?: string;
+  }>;
+  releases: MusicBrainzRelease[];
+}
+
+export const musicBrainzRecording = async (
+  mbid: string,
+): Promise<MusicBrainzRecording> => {
+  const url = new URL(`https://musicbrainz.org/ws/2/recording/${mbid}`);
+  url.searchParams.set("fmt", "json");
+  url.searchParams.set("inc", "artists+releases");
+
+  const requestOptions = {
+    headers: {
+      "User-Agent": "Balancedmusicdl/1.0.0 ( dev@isaid.dev )",
+      Accept: "application/json",
+    },
+  };
+
+  const request = await fetch(url.toString(), requestOptions);
+
+  if (!request.ok) {
+    throw new Error(
+      `MusicBrainz API error: ${request.status} ${request.statusText}`,
+    );
+  }
+
+  const response: MusicBrainzRecording = await request.json();
+  return response;
+};
+
+export const musicBrainzAlbumCover = (releaseMBID: string): string => {
+  return `https://coverartarchive.org/release/${releaseMBID}/front-250`;
+};
