d7e41b6146f92810efe73979443888f304c6c499
SAID <ismael.saidpro@protonmail.com>
2026-04-19 14:18:46 +0400

    [feat] add a patch route for song update

--- back/src/routes/song.ts
+++ back/src/routes/song.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { StatusCodes } from "http-status-codes";
+import {  randomUUID } from "node:crypto";
+import { updateID3tags } from "../utils/id3tags.ts";
+import logger from "../utils/logger.ts";
+import path from "path";
 
 export const songRouter = Router();
 
@@ -48,26 +52,62 @@ songRouter.get(
   },
 );
 
-songRouter.patch(
-  "/songs/:id",
-  async (req: Request, res: Response): Promise<void> => {
+
+songRouter.patch("/songs/:id", async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    const { title, artist, album } = req.body;
+    const { album, title, artist, year, cover } = req.body;
 
     try {
-      const updatedSong = await prisma.song.update({
-        where: { id },
-        data: { artist: artist, album: album, title: title },
-      });
-      res.json(updatedSong);
-    } catch (error) {
-      res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ error: "Song not found or update failed" });
-    }
-  },
-);
+        const updatedSong = await prisma.song.update({
+            where: { id },
+            data: { album, title, artist, year },
+        });
 
+        const songPath = `${process.env.SONGS_DIR}/${updatedSong.fileName || title}`;
+
+        let imagePath: string | undefined;
+
+        if (cover) {
+            try {
+                const dir = process.env.COVERS_ART_DIR || "./covers";
+                const fileName = `${randomUUID()}.jpg`;
+                imagePath = path.join(dir, fileName);
+
+                const response = await fetch(cover, {
+                    method: 'GET',
+                    redirect: 'follow',
+                });
+
+                if (!response.ok) throw new Error(`Fetch failed: ${response.statusText}`);
+
+                const arrayBuffer = await response.arrayBuffer();
+                const buffer = Buffer.from(arrayBuffer);
+
+                await prisma.song.update({
+                    where: { id },
+                    data: { cover: fileName },
+                });
+
+            } catch (error) {
+                logger.error("Cover download/save failed:", error);
+            }
+        }
+
+        updateID3tags(songPath, {
+            album,
+            title,
+            artist,
+            year: year?.toString(),
+            image: imagePath
+        });
+
+
+        res.json(updatedSong);
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Update failed" });
+    }
+});
 songRouter.delete(
   "/songs/:id",
   async (req: Request, res: Response): Promise<void> => {
